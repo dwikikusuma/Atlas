b532c252724143be48d2b875b0e92637ac02505d
dwiki <kusumabriantono@gmail.com>
2025-12-15 21:13:23 +0700

    Refactor main function to establish database connection and improve gRPC server setup

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/dwikikusuma/atlas/pkg/database"
 	"github.com/dwikikusuma/atlas/pkg/kafka"
 	"github.com/dwikikusuma/atlas/pkg/pb/order"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -27,50 +28,21 @@ const (
 func main() {
 	ctx := context.Background()
 
-	dbConfig := database.PostgresConfig{
-		ConnectionURL: postgresURI,
-		MaxConn:       10,
-		MinConn:       2,
-		MaxIdleTime:   300,
-		HealthCheck:   true,
-	}
-
-	connPool, err := database.NewPostgresPool(ctx, dbConfig)
+	connPool, err := connectDB(ctx)
 	if err != nil {
 		log.Fatalf("❌ cannot connect to Postgres: %v", err)
 	}
 	defer connPool.Close()
-	log.Println("✅ Connected to Postgres")
 
 	producer := kafka.NewProducer([]string{kafkaBroker})
 	log.Println("✅ Connecting to Producer...")
 
 	sqlcDB := db.New(connPool)
 	svc := service.NewOrderService(sqlcDB, producer)
+	startConsumer(ctx, sqlcDB)
 
 	grpcServer := grpc.NewServer()
-	order.RegisterOrderServiceServer(grpcServer, svc)
-	reflection.Register(grpcServer)
-
-	dispatchConsumer := kafka.NewConsumer([]string{kafkaBroker}, dispatchGroup, dispatchTopic)
-	setDriverConsumer := service.NewOrderWorker(dispatchConsumer, sqlcDB)
-	go func() {
-		if err = setDriverConsumer.Start(ctx); err != nil {
-			log.Fatalf("❌ order worker failed: %v", err)
-		}
-	}()
-
-	listener, err := net.Listen("tcp", grpcPort)
-	if err != nil {
-		log.Fatalf("❌ cannot create listener: %v", err)
-	}
-	go func() {
-		log.Printf("🚀 Order gRPC server listening on %s", listener.Addr().String())
-		err = grpcServer.Serve(listener)
-		if err != nil {
-			log.Fatalf("❌ cannot start grpc server: %v", err)
-		}
-	}()
+	startGRPCServer(grpcServer, svc)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
@@ -83,4 +55,49 @@ func main() {
 
 	connPool.Close()
 	log.Println("✅ Postgres connection closed")
+}
+
+func connectDB(ctx context.Context) (*pgxpool.Pool, error) {
+	dbConfig := database.PostgresConfig{
+		ConnectionURL: postgresURI,
+		MaxConn:       10,
+		MinConn:       2,
+		MaxIdleTime:   300,
+		HealthCheck:   true,
+	}
+
+	connPool, err := database.NewPostgresPool(ctx, dbConfig)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("✅ Connected to Postgres")
+	return connPool, nil
+}
+
+func startConsumer(ctx context.Context, sqlcDB *db.Queries) {
+	dispatchConsumer := kafka.NewConsumer([]string{kafkaBroker}, dispatchGroup, dispatchTopic)
+	setDriverConsumer := service.NewOrderWorker(dispatchConsumer, sqlcDB)
+	go func() {
+		if err := setDriverConsumer.Start(ctx); err != nil {
+			log.Fatalf("❌ order worker failed: %v", err)
+		}
+	}()
+}
+
+func startGRPCServer(grpcServer *grpc.Server, svc *service.Service) {
+
+	order.RegisterOrderServiceServer(grpcServer, svc)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatalf("❌ cannot create listener: %v", err)
+	}
+	go func() {
+		log.Printf("🚀 Order gRPC server listening on %s", listener.Addr().String())
+		err = grpcServer.Serve(listener)
+		if err != nil {
+			log.Fatalf("❌ cannot start grpc server: %v", err)
+		}
+	}()
 }
